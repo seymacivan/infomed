484e52d69b8a3c51d51452e48ad2ada9ca8fbaf7
seymacivan <seymacivan97@gmail.com>
2024-03-26 22:23:11 +0300

    Initialized infomed spring boot project.

---
+++ backend/src/test/java/com/example/infomed/InfomedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
